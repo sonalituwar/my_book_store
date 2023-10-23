cab0a102672789d8ebcc0ab1e95b183aa16663c7
sonali <tuwar.sonali@gmail.com>
2023-10-23 12:07:41 +0530

    Initial Commit

---
+++ src/main/java/com/project/my_book_store/MyBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.my_book_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/my_book_store/MyBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.my_book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
